65f09745d46e58a70a6f87d82293b5c81eda2987
Hephaisto-dev <hephaistopl@gmail.com>
2021-05-14 20:30:05 +0200

    Complete IStuffManager.java

--- src/main/java/io/github/ph1lou/werewolfapi/IStuffManager.java
+++ src/main/java/io/github/ph1lou/werewolfapi/IStuffManager.java
@@ -9,17 +9,59 @@ import java.util.UUID;
 
 public interface IStuffManager {
 
+    /**
+     * return the extra loots looted when a playerWW die
+     * @return a list of ItemStack
+     */
     List<ItemStack> getDeathLoot();
+
+    /**
+     * Get the Start Loot inventory
+     * @return an Inventory
+     */
     Inventory getStartLoot();
+
+    /**
+     * clear out the current death loot list
+     */
     void clearDeathLoot();
+
+    /**
+     * clear out the current inventory of start loot
+     */
     void clearStartLoot();
+
+    /**
+     * add the specified ItemStack to the current death loot list
+     * @param i
+     */
     void addDeathLoot(ItemStack i);
     void loadStuffChill();
     void loadAllStuffMeetUP();
     void loadAllStuffDefault();
+
+    /**
+     * load roles stuff and stuff start and death from the specified file
+     * @param configName the specified file
+     */
     void load(String configName);
+
+    /**
+     * save  roles stuff and stuff start and death in the specified file
+     * @param configName the specified file
+     */
     void save(String configName);
+
+    /**
+     * get the stuff of a role depending on the role key
+     * @return a map of role key as key and the stuff of the role as value
+     */
     Map<String, List<ItemStack>> getStuffRoles();
+
+    /**
+     * get the stuff inventory of a player in stuff edition mode
+     * @return a map of UUID as key and Inventory as value
+     */
     Map<UUID, Inventory> getTempStuff();
 
 }
