ccc12a90cf916872bfe28eebdd055ee712c2af8f
Hephaisto-dev <hephaistopl@gmail.com>
2021-05-15 21:01:44 +0200

    Complete java doc for ILover.java

--- src/main/java/io/github/ph1lou/werewolfapi/ILover.java
+++ src/main/java/io/github/ph1lou/werewolfapi/ILover.java
@@ -4,12 +4,29 @@ import java.util.List;
 
 public interface ILover {
 
+    /**
+     * Get the list of lovers
+     * @return a List of PlayerWW
+     */
     List<? extends IPlayerWW> getLovers();
 
+    /**
+     * Return true if the lover key match the given one
+     * @param key the given key
+     * @return a boolean
+     */
     boolean isKey(String key);
 
+    /**
+     * Get the key of the lover
+     * @return a String
+     */
     String getKey();
 
+    /**
+     * Return true if the lover is alive
+     * @return a boolean
+     */
     boolean isAlive();
 
     /**
