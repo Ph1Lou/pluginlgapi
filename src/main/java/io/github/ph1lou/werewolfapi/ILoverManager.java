13e93a6e1db2b19374ab60b45be006a2ad6c0dd7
Hephaisto-dev <hephaistopl@gmail.com>
2021-05-15 20:55:23 +0200

    Complete javadoc for ILoverManager.java.java

--- src/main/java/io/github/ph1lou/werewolfapi/ILoverManager.java
+++ src/main/java/io/github/ph1lou/werewolfapi/ILoverManager.java
@@ -4,11 +4,26 @@ import java.util.List;
 
 public interface ILoverManager {
 
+    /**
+     * Get all lovers
+     * @return a List of lovers
+     */
     List<? extends ILover> getLovers();
 
+    /**
+     * Remove a lover
+     * @param ILover the lover to remove
+     */
     void removeLover(ILover ILover);
 
+    /**
+     * Add a lover
+     * @param ILover the lover to add
+     */
     void addLover(ILover ILover);
 
+    /**
+     * Distributes the lovers in the game
+     */
     void repartition();
 }
